ec6ede20aea34154675f77e7963bb1c3fc524ace
Adewale Salami <wales.abbe@gmail.com>
2025-06-08 13:51:09 -0600

    Fix: use direct imports for ButtonProps and LayoutProps to satisfy checker

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
-import { type LayoutProps } from "@/interface";
+import { LayoutProps } from "@/interface";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
