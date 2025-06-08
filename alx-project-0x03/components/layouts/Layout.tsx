f415c3f02a7667d9f699bce1677f07d6d72f81dd
Adewale Salami <wales.abbe@gmail.com>
2025-06-08 13:42:32 -0600

    Refactor: Move interfaces to central interface/index.ts

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -2,9 +2,7 @@ import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
-interface LayoutProps {
-  children: ReactNode;
-}
+import { type LayoutProps } from "@/interface";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
