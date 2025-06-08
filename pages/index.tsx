6a158201836d5b334ffdf52474d269921e8dd5a6
Adewale Salami <wales.abbe@gmail.com>
2025-06-07 22:58:40 -0600

    Add Montserrat Google font to global.css and update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
-import Head from "next/head";
-import Layout from "@/components/layouts/Layout";
-
-export default function Home() {
+const Home = () => {
   return (
-    <Layout>
-      <Head>
-        <title>Splash App</title>
-      </Head>
-      <section className="container mx-auto py-12 px-4">
-        <h1 className="text-4xl font-bold text-gray-800">Welcome to Splash App!</h1>
-        <p className="mt-4 text-gray-600">Your all-in-one solution platform.</p>
-      </section>
-    </Layout>
+    <div>
+      <h1> Welcome to Splash App </h1>
+    </div>
   );
-}
+};
+
+export default Home;
